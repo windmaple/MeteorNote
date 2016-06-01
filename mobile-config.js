e116660e82ebfef089617db4f2f7e6946ec34043
Wei Wei <windmaple@gmail.com>
2016-06-01 08:43:28 +0800

    update icons and splashes using the nice tool 'meteor-assets'

--- mobile-config.js
+++ mobile-config.js
@@ -11,40 +11,42 @@ App.info({
 });
 
 App.icons({
-  // iOS
-  'iphone_2x': 'resources/icons/icon-60x60@2x.png',
-  'iphone_3x': 'resources/icons/icon-60x60@3x.png',
-  'ipad': 'resources/icons/icon-76x76.png',
-  'ipad_2x': 'resources/icons/icon-76x76@2x.png',
-
-  // Android
-  'android_mdpi': 'resources/icons/icon-48x48.png',
-  'android_hdpi': 'resources/icons/icon-72x72.png',
-  'android_xhdpi': 'resources/icons/icon-96x96.png',
+  "iphone_2x": "resources/icons/iphone_2x.png", // 120x120
+  "iphone_3x": "resources/icons/iphone_3x.png", // 180x180
+  "ipad": "resources/icons/ipad.png", // 76x76
+  "ipad_2x": "resources/icons/ipad_2x.png", // 152x152
+  "ipad_pro": "resources/icons/ipad_pro.png", // 167x167
+  "ios_settings": "resources/icons/ios_settings.png", // 29x29
+  "ios_settings_2x": "resources/icons/ios_settings_2x.png", // 58x58
+  "ios_settings_3x": "resources/icons/ios_settings_3x.png", // 87x87
+  "ios_spotlight": "resources/icons/ios_spotlight.png", // 40x40
+  "ios_spotlight_2x": "resources/icons/ios_spotlight_2x.png", // 80x80
+  "android_mdpi": "resources/icons/android_mdpi.png", // 48x48
+  "android_hdpi": "resources/icons/android_hdpi.png", // 72x72
+  "android_xhdpi": "resources/icons/android_xhdpi.png", // 96x96
+  "android_xxhdpi": "resources/icons/android_xxhdpi.png", // 144x144
+  "android_xxxhdpi": "resources/icons/android_xxxhdpi.png" // 192x192
 });
 
 App.launchScreens({
-  // iOS
-  'iphone_2x': 'resources/splash/ios/Default@2x~iphone.png',
-
-  'iphone5': 'resources/splash/ios/Default-568h@2x~iphone.png',
-  'iphone6': 'resources/splash/ios/Default-667h.png',
-  'iphone6p_portrait': 'resources/splash/ios/Default-736h.png',
-  'iphone6p_landscape': 'resources/splash/ios/Default-Landscape-736h.png',
-
-  'ipad_portrait': 'resources/splash/ios/Default-Portrait~ipad.png',
-  'ipad_portrait_2x': 'resources/splash/ios/Default-Portrait@2x~ipad.png',
-  'ipad_landscape': 'resources/splash/ios/Default-Landscape~ipad.png',
-  'ipad_landscape_2x': 'resources/splash/ios/Default-Landscape@2x~ipad.png',
-
-  // Android
-  'android_mdpi_portrait': 'resources/splash/android/drawable-port-mdpi/screen.png',
-  'android_mdpi_landscape': 'resources/splash/android/drawable-land-mdpi/screen.png',
-  'android_hdpi_portrait': 'resources/splash/android/drawable-port-hdpi/screen.png',
-  'android_hdpi_landscape': 'resources/splash/android/drawable-land-hdpi/screen.png',
-  'android_xhdpi_portrait': 'resources/splash/android/drawable-port-xhdpi/screen.png',
-  'android_xhdpi_landscape': 'resources/splash/android/drawable-land-xhdpi/screen.png',
-});
+  "iphone_2x": "resources/splashes/iphone_2x.png", // 640x490
+  "iphone5": "resources/splashes/iphone5.png", // 640x1136
+  "iphone6": "resources/splashes/iphone6.png", // 750x1334
+  "iphone6p_portrait": "resources/splashes/iphone6p_portrait.png", // 2208x1242
+  "iphone6p_landscape": "resources/splashes/iphone6p_landscape.png", // 2208x1242
+  "ipad_portrait": "resources/splashes/ipad_portrait.png", // 768x1024
+  "ipad_portrait_2x": "resources/splashes/ipad_portrait_2x.png", // 1536x2048
+  "ipad_landscape": "resources/splashes/ipad_landscape.png", // 1024x768
+  "ipad_landscape_2x": "resources/splashes/ipad_landscape_2x.png", // 2048x1536
+  "android_mdpi_portrait": "resources/splashes/android_mdpi_portrait.png", // 320x480
+  "android_mdpi_landscape": "resources/splashes/android_mdpi_landscape.png", // 480x320
+  "android_hdpi_portrait": "resources/splashes/android_hdpi_portrait.png", // 480x800
+  "android_hdpi_landscape": "resources/splashes/android_hdpi_landscape.png", // 800x480
+  "android_xhdpi_portrait": "resources/splashes/android_xhdpi_portrait.png", // 720x1280
+  "android_xhdpi_landscape": "resources/splashes/android_xhdpi_landscape.png", // 1280x720
+  "android_xxhdpi_portrait": "resources/splashes/android_xxhdpi_portrait.png", // 1080x1440
+  "android_xxhdpi_landscape": "resources/splashes/android_xxhdpi_landscape.png" // 1440x1080
+})
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
 App.setPreference('StatusBarBackgroundColor', '#000000');
